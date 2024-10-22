abf2fac201b0aaf8858b18fdecd38f0477e0cab3
Jie-Ting Jiang <stu87282@gmail.com>
2024-10-23 02:34:10 +0800

    add a tiny little basic stylesheet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,12 +17,13 @@
 <style>
 	.app {
 		margin: 0;
-		padding: 16px;
+		padding: 0;
 		width: 100vw;
 		height: 100vh;
 		background-color: black;
 		color: white;
 		box-sizing: border-box;
+		overflow: hidden;
 	}
 	:global(body) {
 		margin: 0;
